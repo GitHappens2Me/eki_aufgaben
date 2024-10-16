a961531c95abcf4ea2c1da541ed6864e2f011739
GitHappens2Me <Spirou99@gmx.de>
2024-10-16 10:51:06 +0200

    Fehler in Bonusübung 1 Aufgabe 1 behoben

--- bonus_uebungen/uebung_1/aufgabe_1/übung1_1.py
+++ bonus_uebungen/uebung_1/aufgabe_1/übung1_1.py
@@ -2,38 +2,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x1 = np.array([-4, -3, -2, -1, 0, 2, 4, 5, 7, 8])
-x2 = np.array([ 5,  6,  3,  2, 1,-1,-4,-5,-2,-3])
-Y  = np.array([ 0,  0,  0,  0, 0, 1, 1, 1, 1, 1])
+coords_x1 = np.array([-4, -3, -2, -1, 0, 2, 4, 5, 7, 8])
+coords_x2 = np.array([ 5,  6,  3,  2, 1,-1,-4,-5,-2,-3])
+Y  = np.array       ([-1, -1, -1, -1,-1, 1, 1, 1, 1, 1])
+
+# Plot Points
+plt.scatter(coords_x1[Y== -1], coords_x2[Y == -1], color='blue')
+plt.scatter(coords_x1[Y == 1], coords_x2[Y == 1], color='red')
+
 
 # X-Matrix
-X = np.array([  [1, -4, 16, 5],
-                [1, -3, 9, 6],
-                [1, -2, 4, 3],
-                [1, -1, 1, 2],
-                [1, 0, 0, 1],
-                [1, 2, 4, -1],
-                [1, 4, 16, -4],
-                [1, 5, 25, -5],
-                [1, 7, 49, -2],
-                [1, 8, 64, -3]])
+df_dw0 = np.ones_like(Y)
+df_dw1 = coords_x1
+df_dw2 = coords_x1**2
+df_dw3 = coords_x2
+
+X = np.stack([df_dw0, df_dw1, df_dw2, df_dw3], axis=1)
 
 # Omega
-W = np.linalg.inv(X.T @ X) @X.T @ Y
+W = np.linalg.inv(X.T @ X) @ X.T @ Y
 
 print("X:\n",X)
 print("Y:",Y)
 print("W:",W)
 
 
-# Plot Points
-plt.scatter(x1[Y== 0], x2[Y == 0], color='blue')
-plt.scatter(x1[Y == 1], x2[Y == 1], color='red')
-
 # Plot the Parabel
-lin_x1 = np.linspace(min(x1)- 1, max(x1)+ 1, 100)
-x2_boundary = -(W[0] + W[1] * lin_x1 + W[2] * (lin_x1 ** 2)) / W[3]  
-plt.plot(lin_x1, x2_boundary, color='green')
+#lin_x1 = np.linspace(min(x1)- 1, max(x1)+ 1, 100)
+#x2_boundary = -(W[0] + W[1] * lin_x1 + W[2] * (lin_x1 ** 2)) / W[3]  
+#plt.plot(lin_x1, x2_boundary, color='green')
+
+x1, x2 = np.meshgrid(np.linspace(-5,10,50), np.linspace(-5,10,50))
+z = W[0] + W[1] * x1 + W[2] * x1 * x1 + W[3] * x2
+z = np.clip(z, -1, 1)
+print(np.min(z), np.max(z))
+blue_indices = (Y < 0.0)
+red_indices = (Y > 0.0)
+plt.contourf(x1, x2, z, levels=np.linspace(-1,1,20), cmap="coolwarm", alpha=.2)
+plt.contour(x1, x2, z, levels=[0.0], colors=["k"])
+plt.plot(coords_x1[blue_indices], coords_x2[blue_indices], 'bo')
+plt.plot(coords_x1[red_indices], coords_x2[red_indices], 'ro')
+plt.xlim((-5,10))
+plt.ylim((-5,10))
 
 
 plt.xlabel('x1')
