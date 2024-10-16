a648e0f9ff043031e2d0d8f9913e5f34a810b5ff
GitHappens2Me <Spirou99@gmx.de>
2024-10-16 14:37:52 +0200

    Visualisierung und Code der Bonusübung Aufgabe 1 verbessert

--- bonus_uebungen/uebung_1/aufgabe_1/übung1_1.py
+++ bonus_uebungen/uebung_1/aufgabe_1/übung1_1.py
@@ -1,55 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-coords_x1 = np.array([-4, -3, -2, -1, 0, 2, 4, 5, 7, 8])
-coords_x2 = np.array([ 5,  6,  3,  2, 1,-1,-4,-5,-2,-3])
+# Datensatz: 
+x1 = np.array([-4, -3, -2, -1, 0, 2, 4, 5, 7, 8])
+x2 = np.array([ 5,  6,  3,  2, 1,-1,-4,-5,-2,-3])
 Y  = np.array       ([-1, -1, -1, -1,-1, 1, 1, 1, 1, 1])
 
-# Plot Points
-plt.scatter(coords_x1[Y== -1], coords_x2[Y == -1], color='blue')
-plt.scatter(coords_x1[Y == 1], coords_x2[Y == 1], color='red')
 
-
-# X-Matrix
+# X-Matrix aufstellen 
 df_dw0 = np.ones_like(Y)
-df_dw1 = coords_x1
-df_dw2 = coords_x1**2
-df_dw3 = coords_x2
-
+df_dw1 = x1
+df_dw2 = x1**2
+df_dw3 = x2
 X = np.stack([df_dw0, df_dw1, df_dw2, df_dw3], axis=1)
 
-# Omega
+# Omega berechnen:
 W = np.linalg.inv(X.T @ X) @ X.T @ Y
 
-print("X:\n",X)
-print("Y:",Y)
-print("W:",W)
+# Ausgabe der Resultate:
+print(f"Die X-Matrix hat folgene Form:\n{X}\n")
+print(f"Die berechneten Paramter werte sind:{W}\n")
 
 
-# Plot the Parabel
-#lin_x1 = np.linspace(min(x1)- 1, max(x1)+ 1, 100)
-#x2_boundary = -(W[0] + W[1] * lin_x1 + W[2] * (lin_x1 ** 2)) / W[3]  
-#plt.plot(lin_x1, x2_boundary, color='green')
 
-x1, x2 = np.meshgrid(np.linspace(-5,10,50), np.linspace(-5,10,50))
-z = W[0] + W[1] * x1 + W[2] * x1 * x1 + W[3] * x2
+# Visualisierung der Trennebene und der Datenpunkte
+x1_mesh, x2_mesh = np.meshgrid(np.linspace(-5,10,50), np.linspace(-5,10,50))
+z = W[0] + W[1] * x1_mesh + W[2] * x1_mesh * x1_mesh + W[3] * x2_mesh
 z = np.clip(z, -1, 1)
-print(np.min(z), np.max(z))
-blue_indices = (Y < 0.0)
-red_indices = (Y > 0.0)
-plt.contourf(x1, x2, z, levels=np.linspace(-1,1,20), cmap="coolwarm", alpha=.2)
-plt.contour(x1, x2, z, levels=[0.0], colors=["k"])
-plt.plot(coords_x1[blue_indices], coords_x2[blue_indices], 'bo')
-plt.plot(coords_x1[red_indices], coords_x2[red_indices], 'ro')
+
+plt.contourf(x1_mesh, x2_mesh, z, levels=np.linspace(-1,1,20), cmap="coolwarm", alpha=.2)
+plt.contour(x1_mesh, x2_mesh, z, levels=[0.0], colors=["k"])
+plt.plot(x1[(Y < 0.0)], x2[(Y < 0.0)], 'bs')  # Blaue Vierecke
+plt.plot(x1[(Y > 0.0)], x2[(Y > 0.0)], 'r^')  # Rote Dreiecke
 plt.xlim((-5,10))
 plt.ylim((-5,10))
-
-
 plt.xlabel('x1')
 plt.ylabel('x2')
 plt.title('Lineare Klassifikation')
-plt.legend()
-
 plt.grid(True)
 plt.show()
