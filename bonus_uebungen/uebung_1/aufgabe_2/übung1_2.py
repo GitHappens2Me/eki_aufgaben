d0d6bbab4363d5e46589233a30b622d8c0701cbe
GitHappens2Me <Spirou99@gmx.de>
2024-10-16 12:39:35 +0200

    added animation to bonus exercise

--- bonus_uebungen/uebung_1/aufgabe_2/übung1_2.py
+++ bonus_uebungen/uebung_1/aufgabe_2/übung1_2.py
@@ -15,26 +15,28 @@ def sigmoid(x):
 
 def p_lecker(kunde, W):
     return (
-        sigmoid(W[0] * Zucker[kunde] + W[1] * AciBiber[kunde] + W[2] * Salz[kunde] + W[3])
+        sigmoid(model(kunde, W))
     )
 
-
+def model(kunde, W):
+    return W[0] * Zucker[kunde] + W[1] * AciBiber[kunde] + W[2] * Salz[kunde] + W[3]
+    
 
 # Vorherhsagen berechnen:
 for kunde in range(N_kunden):
-    print(f"Kunde {kunde}: Zucker: {Zucker[kunde]}, AciBiber: {AciBiber[kunde]}, Salz: {Salz[kunde]}, Ergebnis: {Lecker[kunde]}, Vorhersage: {p_lecker(kunde, W)}")
-
+    print(f"Kunde {kunde}: Zucker: {Zucker[kunde]}, AciBiber: {AciBiber[kunde]}, Salz: {Salz[kunde]}, Ergebnis: {Lecker[kunde]}, Vorhersage: {round(model(kunde, W), 2)} , Vorhersage%: {round(p_lecker(kunde, W), 3)* 100}%")
 
 
 # Likelihood berechnen:
 
-def likelihood():
+def likelihood(W):
     likelihood = 1
     for kunde in range(N_kunden):
         likelihood *= (np.power(p_lecker(kunde, W), Lecker[kunde]) * np.power(1 - p_lecker(kunde, W), 1 - Lecker[kunde]))
     return likelihood
 
-print(f"Likelihood: {likelihood()}")
+print(f"Likelihood: {likelihood(W)}")
+
 
 
 # Gradienten bestimmen:
@@ -42,27 +44,65 @@ print(f"Likelihood: {likelihood()}")
 def grad(W):
     return np.array([
         #korrekte indizes bei Zucker, A.b. & Salz? 
-        (Lecker[0] - p_lecker(0, W) * Zucker[0]),
-        (Lecker[1] - p_lecker(1, W) * AciBiber[1]),
-        (Lecker[2] - p_lecker(2, W) * Salz[2]),
-        (Lecker[3] - p_lecker(3, W) * 1 )
+        sum([(Lecker[i] - p_lecker(i, W)) * Zucker[i] for i  in range(N_kunden)]),
+        sum([(Lecker[i] - p_lecker(i, W)) * AciBiber[1] for i  in range(N_kunden)]),
+        sum([(Lecker[i] - p_lecker(i, W)) * Salz[2] for i  in range(N_kunden)]),
+        sum([(Lecker[i] - p_lecker(i, W)) * 1 for i  in range(N_kunden)])
     ])
 
+# [(Lecker[i] - p_lecker(i, W) * W[0]) for i  in range(N_kunden)],
 print(f"Gradient: {grad(W)}")
 
 # Gradienten Abstieg:
 
-def gradient_descend(n, W, lern_rate):
+def gradient_descend(n, W, lern_rate, log):
+   
+    
     for i in range(n):
+        #print(f"{i}: {likelihood(W)}, {grad(W)}")
+        
+        if(i % 10 == 0):
+            plt.clf()
+
+            zucker, aciBiber = np.meshgrid(np.linspace(0, 10, 200), np.linspace(0, 10, 200))
+            z =  W[0] * zucker + W[1] * aciBiber + W[3]
+            plt.contourf(zucker, aciBiber, z, levels=[-10,0,10], alpha=.2, cmap="coolwarm")
+            plt.contour(zucker, aciBiber, z, levels=[0.0], colors=["k"])
+            plt.xlabel('Zucker')
+            plt.ylabel('AciBiber')
+
+            plt.pause(0.1)
+
+        log.append(likelihood(W))
         W = W + lern_rate * grad(W) 
     return W
+
+
+steps = 5000
 
 # Für Übung: n = 1
 # Frage: Warum bleibt Kunde 0 schlecht vorhergesagt?
 print(f"Omega: {W}")
-W = gradient_descend(n = 100000, W = W, lern_rate= 0.1)
+
+x = np.array([step for step in range(steps)])
+y = []
+
+
+
+W = gradient_descend(n = steps, W = W, lern_rate= 0.01, log = y)
 print(f"Omega: {W}")
+
+#plt.plot(x, y)
+#plt.grid()
+
+
+
+
 for kunde in range(N_kunden):
-    print(f"Kunde {kunde}: Zucker: {Zucker[kunde]}, AciBiber: {AciBiber[kunde]}, Salz: {Salz[kunde]}, Ergebnis: {Lecker[kunde]}, Vorhersage: {p_lecker(kunde, W)}")
+    print(f"Kunde {kunde}: Zucker: {Zucker[kunde]}, AciBiber: {AciBiber[kunde]}, Salz: {Salz[kunde]}, Ergebnis: {Lecker[kunde]}, Vorhersage: {round(model(kunde, W), 2)} , Vorhersage%: {round(p_lecker(kunde, W), 3)* 100}%")
+
+
+plt.show()
+
 
 
